40b6672075aa3844e14dad4a5747d4d252fde4af
Ivo Maixner <ivo.maixner@gmail.com>
2011-12-14 20:38:51 +0100

    Add CleanDbIT.

---
+++ src/test/java/it/CleanDbIT.java
@@ -0,0 +1,17 @@
+package it;
+
+import net.sf.lightair.LightAir;
+import net.sf.lightair.annotation.Setup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(LightAir.class)
+@Setup
+public class CleanDbIT {
+
+	@Test
+	public void ok() {
+	}
+
+}
